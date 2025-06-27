e62e8faa45c5009e7f93ca01e942c975ec07bee1
basush <basavaraj.hadagal@gmail.com>
2025-06-27 12:38:10 +0530

    1. removed old route endpoint in API Gateway Config 2. changed file store path 3. changed config path

--- api-gateway/src/main/java/com/in128minutes/microservices/api_gateway/APIGatewayConfiguration.java
+++ api-gateway/src/main/java/com/in128minutes/microservices/api_gateway/APIGatewayConfiguration.java
@@ -18,33 +18,18 @@ public class APIGatewayConfiguration {
 
         GatewayFilter gatewayFilter = authenticationFilter.apply(new AuthenticationFilter.Config());
 
-        return builder.routes().route(p->p.path("/get")
-                .filters(f->f
-                        .addRequestHeader("MyHeader","MyURI")
-                        .addRequestHeader("MyParam","MyParamValue"))
-                .uri("http://httpbin.org:80"))
-                .route(p->p.path("/currency-exchange/**")
-                        .uri("lb://currency-exchange-service"))
-                .route(p->p.path("/currency-conversion/**")
-                        .uri("lb://currency-conversion-service"))
-                .route(p->p.path("/currency-conversion-feign/**")
-                        .uri("lb://currency-conversion-service"))
-                .route(p->p.path("/currency-conversion-new/**")
-                        .filters(f->f.rewritePath(
-                                "/currency-conversion-new/(?<segment>.*)",
-                                "/currency-conversion-feign/${segment}"
-                        ))
-                        .uri("lb://currency-conversion-service"))
-                .route(p->p.path("/digital-claim/**")
-                        .uri("lb://digital-claim-service"))
+        return builder.routes()
+                .route(p->p.path("/get")
+                    .filters(f->f
+                            .addRequestHeader("MyHeader","MyHeaderValue")
+                            .addRequestHeader("MyParam","MyParamValue"))
+                    .uri("http://httpbin.org:80"))
                 .route(p->p.path("/digi-claim/**")
                         .filters(f->f.filter(gatewayFilter))
                         .uri("lb://digi-claim-service"))
                 .route(p->p.path("/auth/**")
                         .uri("lb://auth-service"))
                 .build();
-
-
     }
 
 }
